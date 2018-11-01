f108c7f27177421e677e7ace00687da3c570f2da
ewhaqh9909 <qhdud0921@naver.com>
2018-11-01 15:50:58 +0900

    06_1_1_1

--- 06_1.c
+++ 06_1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #define ROWS 3
 #define COLS 3
+#include "06_1_1.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void printMatrix(int A[][COLS]);
@@ -24,29 +25,4 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void addMatrix(int A[][COLS],int B[][COLS],int C[][COLS])
-{
-	int i,j;
-	
-	for(i=0;i<ROWS;i++)
-	{
-		for(j=0;j<COLS;j++){
-			C[i][j]=A[i][j]+B[i][j];
-		}
-	}
-	return 1;
-	
-}
 
-void printMatrix(int A[][COLS])
-{
-	int i,j;
-	
-	for (i=0;i<ROWS;i++){
-		for(j=0;j<COLS;j++){
-			printf("%d ",A[i][j]);
-		}printf("\n");
-	}
-	
-	return;
-}
